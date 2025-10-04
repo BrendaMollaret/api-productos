215db978e7c6bb02a657f67660f86c0332a13e36
Brenda Mollaret <mollaretbrenda@gmail.com>
2025-10-04 11:28:40 -0300

    rutas crud

---
+++ routes/productos.js
@@ -0,0 +1,55 @@
+// /routes/productos.js
+const express = require("express");
+const router = express.Router();
+const Producto = require("../models/Producto");
+
+// Obtener todos los productos
+router.get("/productos", async (req, res) => {
+  try {
+    const productos = await Producto.find();
+    res.json(productos);
+  } catch (error) {
+    res.status(500).json({ message: "Error al obtener productos" });
+  }
+});
+
+// POST
+router.post("/productos", async (req, res) => {
+  try {
+    const nuevoProducto = new Producto(req.body);
+    const nuevoProductoGuardado = await nuevoProducto.save();
+    res.status(201).json(nuevoProductoGuardado);
+  } catch (error) {
+    res.status(400).json({ message: "Error al crear producto" });
+  }
+});
+
+// PUT
+router.put("/productos/:id", async (req, res) => {
+  try {
+    const productoActualizado = await Producto.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    );
+    if (!productoActualizado) {
+      return res.status(404).json({ message: "Producto no encontrado" });
+    }
+    res.json(productoActualizado);
+  } catch (error) {
+    res.status(400).json({ message: "Error al actualizar producto" });
+  }
+});
+
+// DELETE
+router.delete("/productos/:id", async (req, res) => {
+  try {
+    const productoEliminado = await Producto.findByIdAndDelete(req.params.id);
+    if (!productoEliminado) {
+      return res.status(404).json({ message: "Producto no encontrado" });
+    }
+    res.json({ message: "Producto eliminado" });
+  } catch (error) {
+    res.status(400).json({ message: "Error al eliminar producto" });
+  }
+});
