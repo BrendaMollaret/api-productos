340a2b1e54009f98cb88de9ee84e9206a3de037a
Brenda Mollaret <89263107+BrendaMollaret@users.noreply.github.com>
2025-10-25 11:47:47 -0300

    Actualizar corsOptions.js

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -2,9 +2,8 @@
 
 const corsOptions = {
   origin: [
-    "http://localhost:5173",
-    "https://front-funcionesbasicas.vercel.app",
-"https://productos-frontend.vercel.app/",
+    "https://productos-frontend.vercel.app/",
+    "https://front-funcionesbasicas.vercel.app"
   ],
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   allowedHeaders: ["Content-Type", "Authorization"],
